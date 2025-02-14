b8a7dcaba3b04c91b5f6a651571b48102874a4a4
Andrés Agosto <alejandroagosto2012@gmail.com>
2025-02-13 18:10:04 -0600

    mezcla1

--- Practica1_G4/server/src/main/java/gt/edu/usac/ingenieria/MediTrack/MediTrackApplication.java
+++ Practica1_G4/server/src/main/java/gt/edu/usac/ingenieria/MediTrack/MediTrackApplication.java
@@ -12,10 +12,8 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 @SpringBootApplication
 @RestController
