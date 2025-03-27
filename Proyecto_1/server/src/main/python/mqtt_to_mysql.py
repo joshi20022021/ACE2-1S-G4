bc0984f1eade1825012ab020b3b0450f6826d8f2
Luis Pichiyá <3502658870401@ingenieria.usac.edu.gt>
2025-03-27 12:32:38 -0600

    IP HostPot

--- Proyecto_1/server/src/main/python/mqtt_to_mysql.py
+++ Proyecto_1/server/src/main/python/mqtt_to_mysql.py
@@ -75,5 +75,5 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("localhost", 1883, 60)
+client.connect("192.168.137.1", 1883, 60)
 client.loop_forever()
